7b0b2440597c06eb760f4aa9fbfea6210da6c84d
Khushali Sathawane <97839552+Khushalisathawane50@users.noreply.github.com>
2023-08-03 11:42:07 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,31 +1,20 @@
-"""program1 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
 from sensor import views
+from django.conf.urls.static import static
+from django.conf import settings
+from sensor.views import ServiceWorker
 
-from django.views.static import serve
-from django.urls import re_path
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('sensor.urls')),
-    path('', include('pwa.urls')),
-
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path("", views.homepage, name="homepage"),
+    path("Login/", views.userlogin, name="Login"),
+    path("Home/", views.home, name="Home"),
+    path("device-register/", views.Dashboard, name="Dashboard"),
+    path("DailyRecords/", views.daily_records, name="DailyRecords"),
+    path("Reports/", views.Reports, name="Reports"),
+    path("logout/", views.logoutuser, name="logoutuser"),
+    path("charts/", views.charts, name="charts"),
+    path("register/", views.register_user, name="register"),
+    path("chartsData/", views.chartsData, name="chartsData"),
+    path('', ServiceWorker.as_view(), name="sw"),
 ]
